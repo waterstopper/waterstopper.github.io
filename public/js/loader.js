4caef7bdbfdd6e6f29adbcd1f96b0806bc5c785f
akononov <akononov@nomia.net>
2024-03-22 16:47:42 +0300

    transfer

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,21 +1,7 @@
-import { byId } from "./util.js";
-
 
 function _set_theme() {
-    let theme = localStorage.getItem("theme") ?? "dark"
-    console.log(theme)
-    console.log(`set themae ${theme}`)
+    let theme = localStorage.getItem("theme") ?? "light"
     document.documentElement.setAttribute('data-theme', theme);
 }
 
 _set_theme()
-
-byId("change-theme-button").onclick = () => {
-    console.log("a")
-    let theme = document.documentElement.getAttribute('data-theme') ?? "light";
-    let new_theme = theme == "light" ? "dark" : "light";
-
-    theme_button.innerText = new_theme;
-    localStorage.setItem(stored.theme, new_theme);
-    _set_theme();
-}
