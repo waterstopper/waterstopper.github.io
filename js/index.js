84327ea98a2c88bb0e3127c9e95548d4a1d78b46
waterstop <waterstop@yandex-team.ru>
2024-08-11 00:21:13 +0300

    added drawings

--- js/index.js
+++ js/index.js
@@ -1,27 +1,40 @@
 import { byClass, byId } from "./utils.js";
 
-/*  */
 let previewDict = await ((await fetch("resources/main/preview.json")).json())
 let preview = byId("preview")
 
-
 Array.from(byClass("hoverable")).forEach(element => {
-    element.onmouseenter = () => {
-        console.log(element.innerText)
-        let previewValues = previewDict[element.innerText]
-        if (previewValues != null) {
-            showPreview(previewValues, element)
-        }
-    }
-    element.onmouseleave = () => {
-        hidePreview()
-    }
+    element.onmouseenter = () => onHover(element);
+    element.ontouchstart = () => onHover(element);
+    
+    element.onmouseleave = () => hidePreview();
+    element.ontouchend = () => hidePreview();
 });
+
+function onHover(element) {
+    let previewValues = previewDict[element.innerText];
+    if (previewValues != null) {
+        showPreview(previewValues, element);
+    }
+}
+
+function isMobile() {
+    return window.screen.height > window.screen.width
+}
+
+let mobile = isMobile();
+
+if(mobile) {
+    preview.style.width = "95%"
+    preview.getElementsByTagName("img")[0].style.width = "100%"
+    preview.getElementsByTagName("div")[0].getElementsByTagName("p")[0].style.fontSize = "xx-large";
+    preview.getElementsByTagName("div")[0].getElementsByTagName("b")[0].style.fontSize = "xx-large";
+}
 
 function showPreview(previewValues, e) {
     let rect = e.getBoundingClientRect()
 
-    preview.style.top = (rect.top - 100) + "px"
+    preview.style.top = mobile ? rect.top + "px" : (rect.top - 100) + "px"
     preview.style.display = "block"
 
     if (previewValues.img != null) {
